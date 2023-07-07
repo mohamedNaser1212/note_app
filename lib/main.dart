0db4645e2d2b6f031119109340bcc9bcb9d748c2
mohamedNaser1212 <nasergamal33e@gmail.com>
2023-07-07 17:51:17 +0300

    Your Message Here

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class NotesApp extends StatelessWidget {
+  const NotesApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      home: ,
+
+    );
+  }
+}
