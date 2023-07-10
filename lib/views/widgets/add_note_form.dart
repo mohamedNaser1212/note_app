f5a71c4df782f0cbef896f4e723cf5156f6bbcf1
mohamedNaser1212 <nasergamal33e@gmail.com>
2023-07-10 13:59:17 +0300

    display data part 2

--- lib/views/widgets/add_note_form.dart
+++ lib/views/widgets/add_note_form.dart
@@ -47,23 +47,26 @@ class _AddNoteFormState extends State<AddNoteForm> {
             height: 60,
           ),
           BlocBuilder<AddNoteCubit, AddNoteState>(
-  builder: (context, state) {
-    return CustomButton(
-            isLoading: state is AddNoteLoading?true:false,
-            onTap: () {
-              if (formKey.currentState!.validate()) {
-                formKey.currentState!.save();
-                var noteModel=NoteModel(date: DateTime.now().toString()
-                    , color: Colors.blue.value, title: title!, subtitle: subtitle!);
-                BlocProvider.of<AddNoteCubit>(context).addNote(noteModel);
-              } else {
-                autovalidateMode = AutovalidateMode.always;
-                setState(() {});
-              }
+            builder: (context, state) {
+              return CustomButton(
+                isLoading: state is AddNoteLoading ? true : false,
+                onTap: () {
+                  if (formKey.currentState!.validate()) {
+                    formKey.currentState!.save();
+                    var noteModel = NoteModel(
+                        date: DateTime.now().toString(),
+                        color: Colors.blue.value,
+                        title: title!,
+                        subtitle: subtitle!);
+                    BlocProvider.of<AddNoteCubit>(context).addNote(noteModel);
+                  } else {
+                    autovalidateMode = AutovalidateMode.always;
+                    setState(() {});
+                  }
+                },
+              );
             },
-          );
-  },
-),
+          ),
           const SizedBox(
             height: 60,
           ),
--- lib/views/widgets/custom_note_item.dart
+++ lib/views/widgets/custom_note_item.dart
@@ -1,33 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+import '../../models/note_model.dart';
 import '../edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
-  const NoteItem({Key? key}) : super(key: key);
-
+  const NoteItem({Key? key, required this.note}) : super(key: key);
+  final NoteModel note;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: (){
         Navigator.push(context, MaterialPageRoute(builder:(context){
-          return EditNoteView();
+          return const EditNoteView();
 
         } ));
       },
       child: Container(
         padding: const EdgeInsets.only(top: 24, bottom: 24, left: 10),
         decoration: BoxDecoration(
-          color: const Color(0xffFFCC80),
+          color:  Color(note.color),
           borderRadius: BorderRadius.circular(16),
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
             ListTile(
-              title: const Text(
-                'Flutter tips',
-                style: TextStyle(
+              title:  Text(
+                note.title,
+                style: const TextStyle(
                   fontSize: 26,
                   color: Colors.black,
                 ),
@@ -35,7 +36,7 @@ class NoteItem extends StatelessWidget {
               subtitle: Padding(
                 padding: const EdgeInsets.only(top: 16.0, bottom: 16),
                 child: Text(
-                  'Build your career with Mohamed Nasser',
+                  note.subtitle,
                   style: TextStyle(
                       fontSize: 18, color: Colors.black.withOpacity(0.5)),
                 ),
@@ -52,7 +53,7 @@ class NoteItem extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.only(right: 24.0),
               child: Text(
-                'May21,2022',
+                note.date,
                 style: TextStyle(
                   color: Colors.black.withOpacity(0.4),
                   fontSize: 16,
